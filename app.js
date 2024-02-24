7bc7b9e5900ba916a5c105850c4e8c92382e46cb
Aryanpatil1710 <151561766+Aryanpatil1710@users.noreply.github.com>
2024-02-24 21:37:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express ();
+const mongoose = require("mongoose");
+const Listing = require("../models/listing.js")
+
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+main()
+ .then(()=> {
+    console.log("connected to DB");
+ })
+ .catch((err)=>{
+    console.log(err);
+ });
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+app.get("/", (req,res)=>{
+    res.send("hi , I am root");
+});
+
+app.get("/testListing", async (req,res)=>{
+    let sampleLisiting = new Listing({
+        title : "My new Villa",
+        description : "By the beach",
+        location : "Calangute, Goa",
+        country : "India",
+    });
+
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("successful testing");
+});
+app.listen(8080 , () => {
+    console.log ( "listening to port 8080" );
+});
